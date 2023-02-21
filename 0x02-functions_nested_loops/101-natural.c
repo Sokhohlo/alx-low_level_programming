a92d1b2a84863635d89e69685d2b0ba32a543f11
Sokhohlo <ssokhohlo@gmail.com>
2023-02-21 13:38:48 -0800

    1011

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,11 +12,15 @@
 int main(void)
 {
 int i, sum = 0;
+
 for (i = 0; i < 1024; i++)
 {
 if ((i % 3) == 0 || (i % 5) == 0)
 sum += i;
 }
-printf("%d ", sum);
+
+printf("%d\n", sum);
+
 return (0);
 }
+
