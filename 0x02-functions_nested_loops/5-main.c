46166600236fa9a680bc8eb4040fa56c408c17c9
Sokhohlo <ssokhohlo@gmail.com>
2023-02-21 11:31:57 -0800

    5-main

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,34 +1,45 @@
 #include "main.h"
 
 /**
-*main - check the code.
+* main - check the code.
 *
-Return: Always 0.
+* Return: Always 0.
 */
+
 int main(void)
+
 {
+
 int r;
+
 r = print_sign(98);
+
 _putchar(',');
 _putchar(' ');
 _putchar(r + '0');
-_putchar('\n');
+_putchar(' ');
+
 r = print_sign(0);
+
 _putchar(',');
 _putchar(' ');
 _putchar(r + '0');
-_putchar('\n');
+_putchar(' ');
+
 r = print_sign(0xff);
+
 _putchar(',');
 _putchar(' ');
 _putchar(r + '0');
-_putchar('\n');
+_putchar(' ');
+
 r = print_sign(-1);
+
 _putchar(',');
 _putchar(' ');
 _putchar(r + '0');
-_putchar('\n');
+_putchar(' ');
+
 return (0);
+
 }
-
-
