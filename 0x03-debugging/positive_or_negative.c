8308ca86bc8afaa0f2c916c7efd024d828f406aa
Sokhohlo <ssokhohlo@gmail.com>
2023-02-22 11:28:48 -0800

    tadk40

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+/**
+ * positive_or_negative - function print  either positive, negative or zero
+ * @i: parameter used
+ * Return: Always 0 (Success)
+ */
+
+void positive_or_negative(int i)
+
+
+{
+
+
+if (i > 0)
+
+{
+
+printf("%d is positive\n", i);
+
+}
+
+if (i < 0)
+
+{
+
+printf("%d is negative\n", i);
+
+}
+
+if (i == 0)
+
+{
+
+printf("%d is zero\n", i);
+
+}
+
+
+}
