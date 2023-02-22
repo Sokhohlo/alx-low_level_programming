f2366df180d295411b0b3b5ec2e9cc5553c024dd
Sokhohlo <ssokhohlo@gmail.com>
2023-02-22 14:38:09 -0800

    ftz

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-i = 0;
+/* comment - of the block of code causing infinite loop. i = 0; */
 
-while (i < 10)
-{
-putchar(i);
-}
+/* while (i < 10) */
+/* { */
+/* putchar(i); */
+/* } */
 
 printf("Infinite loop avoided! \\o/\n");
 
