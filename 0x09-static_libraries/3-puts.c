e723f8e519278f672534463517b61b1f7f00f912
Sokhohlo <ssokhohlo@gmail.com>
2023-03-10 03:50:17 -0800

    donez

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* _puts - Entry Point
+* @str: Length
+* Return: Always 0
 */
-int main(void)
+
+void _puts(char *str)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+printf("%s ", str);
+
 }
