be104555c5a4dbdc9e7ca83bcc4a235befd2f57c
Sokhohlo <ssokhohlo@gmail.com>
2023-03-10 08:45:53 -0800

    zuba

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,30 +1,18 @@
-#include<stdio.h>
+#include "main.h"
 
-void _puts(char *str);
+#include <stdio.h>
 
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+void _puts(char *s)
+
 {
-_puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
 
-/**
-* _puts - prints a string to stdout
-* @str: the string to print
-*/
-void _puts(char *str)
-{
-int i = 0;
+puts(s);
 
-while (str[i] != '\0')
-{
-putchar(str[i]);
-i++;
-}
-putchar('\n');
 }
