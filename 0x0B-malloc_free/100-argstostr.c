b05ecff8111260b9b58a50e389b6d56e06979541
Sokhohlo <ssokhohlo@gmail.com>
2023-03-13 01:50:36 -0700

    does

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,22 +1,3 @@
-
-#include <stdlib.h>
-#include "holberton.h"
-/**
-* free_grid - frees the memory allocate for the grid
-* created by alloc_grid()
-* @grid: grid to free
-* @height: height of the grid
-*/
-void free_grid(int **grid, int height)
-{
-int i;
-if (grid == NULL || height == 0)
-return;
-for (i = 0; i < height; i++)
-free(grid[i]);
-free(grid);
-}
-vi 100-argstostr.c
 #include <stdlib.h>
 #include "holberton.h"
 /**
--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -11,23 +11,3 @@ int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
-vi 0-create_array.c
-#include "holberton.h"
-#include <stdlib.h>
-/**
-* *create_array - creates an array of chars, and initializes
-*  it with a specific char.
-*  @c: char to initialize
-*  @size: number of bytes to allocate
-*
-*  Return: a pointer to the array or null if it fails
-*/
-char *create_array(unsigned int size, char c)
-{
-char *array = malloc(size);
-if (size == 0 || array == 0)
-return (NULL);
-while (size--)
-array[size] = c;
-return (array);
-}
