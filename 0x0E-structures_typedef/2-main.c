4ec5acda85d8b1a280ee614ddf2a6910132fa6fa
Sokhohlo <ssokhohlo@gmail.com>
2023-03-20 10:33:54 -0700

    fyj

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include "dog.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* main - prints the name of the file
+*
+* Return: Always 0 (Success)
+*/
+int ain(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
